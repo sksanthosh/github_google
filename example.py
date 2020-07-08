187d63f2fca090a0c43e455ef43a5d1cbb92df72
santhoshsk <sksanthosh@outlook.com>
2020-07-08 12:54:33 +0000

    changing example.py

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
+def git_operation():
+ print("I m adding example.py file to the remote repository.")
+git_operation()
